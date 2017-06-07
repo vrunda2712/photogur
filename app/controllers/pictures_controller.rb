25291774bbb17d475765b7311bd8c15133bedebb
vrunda2712 <angy.v27@gmail.com>
2017-06-07 15:39:38 -0400

    Add form for adding new pictures by routes, controller and new file

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,7 +3,16 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+
   def show
     @pictures = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    render text: "Received POST request to '/pictures' with the data URL: #{params}"
   end
 end
