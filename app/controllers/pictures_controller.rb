149bf09e5decdb939a5a8c8ba272744ec8934c63
vrunda2712 <angy.v27@gmail.com>
2017-06-07 15:53:17 -0400

    saving new pictures to database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,6 +13,18 @@ class PicturesController < ApplicationController
   end
 
   def create
-    render text: "Received POST request to '/pictures' with the data URL: #{params}"
+    # render text: "Received POST request to '/pictures' with the data URL: #{params}"
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if the picture gets saved, generate a get request to "/pictures" (the index)
+      redirect_to "/pictures"
+    else
+      # otherwise render new.html.erb
+      render :new
+    end
+  end
+
+  def picture_params
+    { title: params[:picture][:title], artist: params[:picture][:artist], url: params[:picture][:url] }
   end
 end
