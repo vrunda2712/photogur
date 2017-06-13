a0aab6b0224d03f9f9d050fa6c21e03d6bf25cbd
vrunda2712 <angy.v27@gmail.com>
2017-06-13 12:22:38 -0400

    changes made in created_before method

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ApplicationRecord
   def self.created_before(time)
-    Picture.where("Created_at > ?", time)
+    Picture.where("Created_at < ?", time)
   end
 
   def self.created_created_in_year(time)
