77112f91091fcdc3096aab96365662cb2fe887dd
vrunda2712 <angy.v27@gmail.com>
2017-06-13 12:40:15 -0400

    use of scope in date queries method

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,13 @@ class Picture < ApplicationRecord
     Picture.where("Created_at < ?", time)
   end
 
-  def self.created_created_in_year(time)
+  def self.created_in_year(time)
     Picture.where("Created_at < ?", time)
   end
+
+  scope :month_old_pictures, -> {created_before(1.month.ago) }
+  scope :created_in_year_2017, -> {created_in_year(Date.new(2017)) }
+  scope :created_in_year_2016, -> {created_in_year(Date.new(2016)) }
+  scope :created_in_year_2015, -> {created_in_year(Date.new(2015)) }
+
 end
