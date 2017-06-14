5998082dcea530337c6b32f07fef4343a1c2fed2
vrunda2712 <angy.v27@gmail.com>
2017-06-13 23:13:41 -0400

    Display error notice to users

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@ class Picture < ApplicationRecord
 
   validates :artist, :url, presence: true
   validates :title, length: {in: 3..20}
-  validates :url, uniqueness :true
+  validates :url, uniqueness: true
 
   def self.created_before(time)
     Picture.where("Created_at < ?", time)
