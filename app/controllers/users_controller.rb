aa0a0e30ad62ed511c7726566d74cce7c39f91e1
vrunda2712 <angy.v27@gmail.com>
2017-06-19 15:12:47 -0400

    Implement users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = user.new(user_params)
+
+    if @user.save
+      redirect_to '/pictures'
+    else
+      render :new
+    end
   end
+
+  def user_params
+    { email: params[:user][:email], password: params[:user][:password], password_confirmation: [:user][:password_confirmation]}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # post 'users' => 'users#create'
-  # get 'users/new' => 'users#new'
+  post 'users' => 'users#create'
+  get 'users/new' => 'users#new'
 
-  resource :users, only: %i(new create)
+  # resource :users, only: %i(new create)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
 
