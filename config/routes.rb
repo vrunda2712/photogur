eae4e3275cc3848fd00ce4e906599411777ae7b5
vrunda2712 <angy.v27@gmail.com>
2017-06-19 16:33:44 -0400

    Remove useless session views, change sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  resource :sessions, only: %i(new create destroy)
   # post 'users/create' => 'users#create'
   # get 'users/new' => 'users#new'
 
