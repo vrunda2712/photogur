c1f66d11b977af8723c7534b2e71a435bce54659
vrunda2712 <angy.v27@gmail.com>
2017-06-19 14:42:30 -0400

    generate controller with : rails g controller Users new create --no-test-framework --no-helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'users' => 'users#create'
+  get 'users/new' => 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
 
