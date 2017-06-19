2cf7fe76dc7d55ab9923fa7e6c102f8fcb438042
vrunda2712 <angy.v27@gmail.com>
2017-06-19 14:46:33 -0400

    removed useless routes and create view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  post 'users' => 'users#create'
-  get 'users/new' => 'users#new'
+  # post 'users' => 'users#create'
+  # get 'users/new' => 'users#new'
 
+  resource :users, only: %i(new create)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607183750) do
+ActiveRecord::Schema.define(version: 20170619183335) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170607183750) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
