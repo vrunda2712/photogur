ad5f2ebd86738a630dfbea7b857365a1f73ffb81
vrunda2712 <angy.v27@gmail.com>
2017-07-18 11:28:39 -0400

    Step-3: Update photo submission process(new form properly submits the new card

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,8 +8,6 @@ class PicturesController < ApplicationController
     @created_in_year_2016 = Picture.created_before(Date.new(2017)).created_after(Date.new(2016))
     @created_in_year_2015 = Picture.created_before(Date.new(2016)).created_after(Date.new(2015))
     @created_in_year_2014 = Picture.created_before(Date.new(2015)).created_after(Date.new(2014))
-
-
   end
 
   def show
